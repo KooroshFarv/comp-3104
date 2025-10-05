a5035170d69bd6f4ee45d1f09e9254f4e9d411a5
kooroshFarv <farvardin.koorosh@gmail.com>
2025-10-05 12:38:39 -0400

    src and public + index js added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const App = () => (
+  <div>
+    <h1>Koorosh Farvardin – 101477772</h1>
+    <p>COMP3104 - Lab 05</p>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
